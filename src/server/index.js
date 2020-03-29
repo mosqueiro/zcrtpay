766256fc2fd42206fba606541bcc61c0ae8eba99
Alex Silva <alexreissilva96@gmail.com>
2020-03-29 19:16:43 -0300

    Create Procfile.

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,11 @@
-import app from './server'
-import config from 'config'
-import { runLastBlockJob } from './jobs/invoices'
-import { runCallbackJob }  from './jobs/callbacks'
+import app from "./server";
+import config from "config";
+import { runLastBlockJob } from "./jobs/invoices";
+import { runCallbackJob } from "./jobs/callbacks";
 
-app.listen(config.listen, _ => console.log("Listening on port",config.listen))
+app.listen(process.env.port || config.listen, _ =>
+  console.log("Listening on port", process.env.port || config.listen)
+);
 
-runLastBlockJob()
-runCallbackJob()
+runLastBlockJob();
+runCallbackJob();
