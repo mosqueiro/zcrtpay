88073bcfc0341b8dafacab96a69394553d42f253
Alex Silva <alexreissilva96@gmail.com>
2020-03-29 19:24:28 -0300

    Add Node Version.

--- src/server/jobs/invoices.js
+++ src/server/jobs/invoices.js
@@ -1,58 +1,60 @@
-import _ from 'lodash'
-import { ZCRT, ethProvider } from '../../contracts'
-import Invoices from '../invoice/manager'
-import { fromBN } from '../../common/utils'
-import config from 'config'
+import _ from "lodash";
+import { ZCRT, ethProvider } from "../../contracts";
+import Invoices from "../invoice/manager";
+import { fromBN } from "../../common/utils";
+import config from "config";
 
 export async function checkPendingInvoices(tokenContract, provider) {
-  const invoices = await Invoices.findPendingInvoices()
-  const curBlock = await provider.getBlockNumber()
-  const tasks = []
-  console.log(`checkPendingInvoices: found ${invoices.length} invoices`)
-  for(let invoice of invoices) {
-    const balanceBN = (await tokenContract.balanceOf(invoice.deposit.address)).toString()
-    const balance = fromBN(balanceBN)
-    const upd = {}
+  const invoices = await Invoices.findPendingInvoices();
+  const curBlock = await provider.getBlockNumber();
+  const tasks = [];
+  console.log(`checkPendingInvoices: found ${invoices.length} invoices`);
+  for (let invoice of invoices) {
+    const balanceBN = (await tokenContract.balanceOf(
+      invoice.deposit.address
+    )).toString();
+    const balance = fromBN(balanceBN);
+    const upd = {};
     if (balance != invoice.paidAmount) {
-      upd.paidAmount = balance
+      upd.paidAmount = balance;
     }
-    const expires = invoice.expires || (invoice.created + 24 * 60 * 60000) // 1 day
+    const expires = invoice.expires || invoice.created + 24 * 60 * 60000; // 1 day
     if (Date.now() > expires) {
-      upd.state = 'expired'
+      upd.state = "expired";
     }
     if (balance >= invoice.totalAmount) {
       if (config.invoices.minConfirmations) {
         if (invoice.confirmBlock) {
           if (curBlock >= invoice.confirmBlock) {
-            upd.state = 'paid'
-          }  
+            upd.state = "paid";
+          }
         } else {
-          upd.state = 'confirming'
-          upd.confirmBlock = curBlock + config.invoices.minConfirmations
+          upd.state = "confirming";
+          upd.confirmBlock = curBlock + config.invoices.minConfirmations;
         }
-      } else
-        upd.state = 'paid'
+      } else upd.state = "paid";
     }
     if (!_.isEmpty(upd)) {
-      console.log(`updating invoice #${invoice._id}`,upd)
-      tasks.push(Invoices.updateInvoice(invoice._id, upd))
+      console.log(`updating invoice #${invoice._id}`, upd);
+      tasks.push(Invoices.updateInvoice(invoice._id, upd));
     }
   }
-  return tasks
+  return tasks;
 }
 export function runLastBlockJob() {
-  let curBlock, lastBlock = 0
+  let curBlock,
+    lastBlock = 0;
   const updateLastBlock = async _ => {
     try {
-      curBlock = await  ethProvider.getBlockNumber()
+      curBlock = await ethProvider.getBlockNumber();
       if (curBlock > lastBlock) {
-        lastBlock = ethProvider.blockNumber
-        await checkPendingInvoices(ZCRT, ethProvider) 
+        lastBlock = ethProvider.blockNumber;
+        await checkPendingInvoices(ZCRT, ethProvider);
       }
-    } catch(e) {
-      console.log(e)
+    } catch (e) {
+      console.log(e);
     }
-    setTimeout(updateLastBlock, 1000)
-  }
-  updateLastBlock()
+    setTimeout(updateLastBlock, 1000);
+  };
+  updateLastBlock();
 }
