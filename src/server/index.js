0782f8004e32dbb448ee5ebab6e68f797c81a73e
Alex Silva <alexreissilva96@gmail.com>
2020-03-29 19:48:54 -0300

    Add lib lodash.

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ import config from "config";
 import { runLastBlockJob } from "./jobs/invoices";
 import { runCallbackJob } from "./jobs/callbacks";
 
+// port
 app.listen(process.env.PORT || config.listen, _ =>
   console.log("Listening on port", process.env.PORT || config.listen)
 );
